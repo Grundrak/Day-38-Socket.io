bb1e799489d2e96d6a0ed06994ddb40ae33ed991
Grundrak <electrovilla66@gmail.com>
2023-10-05 20:13:28 +0100

    code is fixed and work perfectly

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ var http = require('http');
 var app = express();
 const path = require('path');
 
+const messages = [];
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 const server = http.createServer(app);
 const { Server } = require("socket.io");
@@ -18,16 +22,18 @@ io.on('connection', (socket) => {
     console.log(`New client connected: ${socket.id}`);
 
     socket.join('default');
+
+    socket.on('chat message', (data) => {
+      messages.push({ username: data.username, message: data.message });
+      io.emit('chat message', { username: data.username, message: data.message });
+    });
+    
+    socket.on('disconnect', () => {
+      console.log('User disconnected');
+    });
 })
 
 
-socket.on('chat message', (data) => {
-    socket.broadcast.emit('chat message', data.message);
-});
-
-socket.on('disconnect', () => {
-  console.log('User disconnected');
-});
 
 server.listen(3000, () => {
     console.log('Server listening on :3000');
